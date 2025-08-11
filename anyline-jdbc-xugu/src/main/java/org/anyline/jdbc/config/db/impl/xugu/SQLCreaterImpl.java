3ac8ddaca0ade9af4179510111da657358c531c9
jowee <1608262232@qq.com>
2025-08-11 13:54:50 +0800

    feat(anyline-jdbc-xugu): 适配anyline8.5.1 重写批量删除有问题的方法
    - 新增 createDeleteRunSQL 方法，支持生成批量删除的 SQL 语句
    - 增加处理集合类型参数的逻辑，可生成 IN 条件的 SQL
    - 添加空值和空集合的判断，提高 SQL 生成的健壮性
    - 更新项目依赖版本至8.5.1-SNAPSHOT

--- anyline-jdbc-xugu/src/main/java/org/anyline/jdbc/config/db/impl/xugu/SQLCreaterImpl.java
+++ anyline-jdbc-xugu/src/main/java/org/anyline/jdbc/config/db/impl/xugu/SQLCreaterImpl.java
@@ -6,7 +6,12 @@ import org.anyline.jdbc.config.db.OrderStore;
 import org.anyline.jdbc.config.db.SQLCreater;
 import org.anyline.jdbc.config.db.impl.BasicSQLCreaterImpl;
 import org.anyline.jdbc.config.db.run.RunSQL;
+import org.anyline.jdbc.config.db.run.impl.TableRunSQLImpl;
+import org.anyline.jdbc.exception.SQLUpdateException;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
 @Repository("anyline.jdbc.creater.xugu")
 public class SQLCreaterImpl extends BasicSQLCreaterImpl implements SQLCreater{ 
  
@@ -56,5 +61,55 @@ public class SQLCreaterImpl extends BasicSQLCreaterImpl implements SQLCreater{
 			result += ")"; 
 		} 
 		return result; 
-	} 
+	}
+
+	@Override
+	public String getDisKeyFr(){
+		return disKeyFr;
+	}
+	@Override
+	public String getDisKeyTo(){
+		return disKeyTo;
+	}
+
+	@Override
+	public RunSQL createDeleteRunSQL(String table, String key, Object values){
+		if(null == table || null == key || null == values){
+			return null;
+		}
+		StringBuilder builder = new StringBuilder();
+		TableRunSQLImpl run = new TableRunSQLImpl();
+		builder.append("DELETE FROM ").append(table).append(" WHERE ");
+
+		if(values instanceof Collection){
+			Collection cons = (Collection)values;
+			builder.append(getDisKeyFr()).append(key).append(getDisKeyTo());
+			if(cons.size() > 1){
+				builder.append(" IN(");
+				int idx = 0;
+				for(Object obj:cons){
+					if(idx > 0){
+						builder.append(",");
+					}
+					builder.append("?");
+					idx ++;
+					run.addValue(obj);
+				}
+				builder.append(")");
+			}else if(cons.size() == 1){
+				for(Object obj:cons){
+					builder.append("=?");
+					run.addValue(obj);
+				}
+			}else{
+				throw new SQLUpdateException("删除异常:删除条件为空,delete方法不支持删除整表操作.");
+			}
+		}else{
+			builder.append(getDisKeyFr()).append(key).append(getDisKeyTo());
+			builder.append("=?");
+			run.addValue(values);
+		}
+		run.setBuilder(builder);
+		return run;
+	}
 } 
